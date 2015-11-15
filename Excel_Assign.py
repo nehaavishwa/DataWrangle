d58535b876dc33ef31e96a7878fb324ebcf0e378
Nehaa Vishwakarma <nehaavishwa@users.noreply.github.com>
2015-11-15 12:17:00 -0800

    Update Excel_Assign.py

--- Excel_Assign.py
+++ Excel_Assign.py
@@ -24,47 +24,88 @@ def parse_file(datafile):
     #data = None
     maxval_coast = max(sheet.col_values(1, 1, sheet.nrows))
     maxpos_coast = sheet.col_values(1,1, sheet.nrows-1).index(maxval_coast) + 1
-    maxtime_COAST = xlrd.xldate_as_tuple(sheet.cell_value(maxpos_coast,0),0)
+    maxtime_coast = xlrd.xldate_as_tuple(sheet.cell_value(maxpos_coast,0),0)
+    tup_coast = (maxtime_coast)
+    max_YEAR_coast = tup_coast[0]
+    max_MONTH_coast = tup_coast[1]
+    max_DAY_coast = tup_coast[2]
+    max_HOUR_coast = tup_coast[3]
     #print(maxval_coast)
     maxval_EAST = max(sheet.col_values(2, 1, sheet.nrows))
     maxpos_EAST = sheet.col_values(2, 1, sheet.nrows-1).index(maxval_EAST) + 1
     maxtime_EAST = xlrd.xldate_as_tuple(sheet.cell_value(maxpos_EAST, 0), 0)
+    tup_EAST = (maxtime_EAST)
+    max_YEAR_EAST = tup_EAST[0]
+    max_MONTH_EAST = tup_EAST[1]
+    max_DAY_EAST = tup_EAST[2]
+    max_HOUR_EAST = tup_EAST[3]
     #print(maxval_EAST)
+    tup_Far_West = ()
     maxval_FAR_WEST = max(sheet.col_values(3, 1, sheet.nrows))
     maxpos_FAR_WEST = sheet.col_values(3, 1, sheet.nrows-1).index(maxval_FAR_WEST) + 1
     maxtime_FAR_WEST = xlrd.xldate_as_tuple(sheet.cell_value(maxpos_FAR_WEST, 0), 0)
+    tup_Far_West = (maxtime_FAR_WEST)
+    max_YEAR_FAR_WEST = tup_Far_West[0]
+    max_MONTH_FAR_WEST = tup_Far_West[1]
+    max_DAY_FAR_WEST = tup_Far_West[2]
+    max_HOUR_FAR_WEST = tup_Far_West[3]
+    #print(tup_Far_West[0])
     #print(maxval_FAR_WEST)
     maxval_NORTH = max(sheet.col_values(4, 1, sheet.nrows))
     maxpos_NORTH = sheet.col_values(4, 1, sheet.nrows-1).index(maxval_NORTH) + 1
     maxtime_NORTH = xlrd.xldate_as_tuple(sheet.cell_value(maxpos_NORTH, 0), 0)
+    tup_NORTH = (maxtime_NORTH)
+    max_YEAR_NORTH = tup_NORTH[0]
+    max_MONTH_NORTH = tup_NORTH[1]
+    max_DAY_NORTH = tup_NORTH[2]
+    max_HOUR_NORTH = tup_NORTH[3]
     #print(maxval_NORTH)
     maxval_NORTH_C = max(sheet.col_values(5, 1, sheet.nrows))
     maxpos_NORTH_C = sheet.col_values(5, 1, sheet.nrows-1).index(maxval_NORTH_C) + 1
     maxtime_NORTH_C = xlrd.xldate_as_tuple(sheet.cell_value(maxpos_NORTH_C, 0), 0)
+    tup_NORTH_C  = (maxtime_NORTH_C)
+    max_YEAR_NORTH_C = tup_NORTH_C[0]
+    max_MONTH_NORTH_C = tup_NORTH_C[1]
+    max_DAY_NORTH_C = tup_NORTH_C[2]
+    max_HOUR_NORTH_C = tup_NORTH_C[3]
     #print(maxval_NORTH_C)
     maxval_SOUTHERN = max(sheet.col_values(6, 1, sheet.nrows))
     maxpos_SOUTHERN = sheet.col_values(6, 1, sheet.nrows-1).index(maxval_SOUTHERN) + 1
     maxtime_SOUTHERN = xlrd.xldate_as_tuple(sheet.cell_value(maxpos_SOUTHERN, 0), 0)
+    tup_SOUTHERN  = (maxtime_SOUTHERN)
+    max_YEAR_SOUTHERN = tup_SOUTHERN[0]
+    max_MONTH_SOUTHERN = tup_SOUTHERN[1]
+    max_DAY_SOUTHERN = tup_SOUTHERN[2]
+    max_HOUR_SOUTHERN = tup_SOUTHERN[3]
     #print(maxval_SOUTHERN)
     maxval_SOUTH_C = max(sheet.col_values(7, 1, sheet.nrows))
     maxpos_SOUTH_C = sheet.col_values(7, 1, sheet.nrows-1).index(maxval_SOUTH_C) + 1
     maxtime_SOUTH_C = xlrd.xldate_as_tuple(sheet.cell_value(maxpos_SOUTH_C, 0), 0)
+    tup_SOUTH_C  = (maxtime_SOUTH_C)
+    max_YEAR_SOUTH_C = tup_SOUTH_C[0]
+    max_MONTH_SOUTH_C = tup_SOUTH_C[1]
+    max_DAY_SOUTH_C = tup_SOUTH_C[2]
+    max_HOUR_SOUTH_C = tup_SOUTH_C[3]
     #print(maxval_SOUTH_C)
-    maxval_West = max(sheet.col_values(8, 1, sheet.nrows))
-    maxpos_WEST = sheet.col_values(8, 1, sheet.nrows-1).index(maxval_West) + 1
+    maxval_WEST = max(sheet.col_values(8, 1, sheet.nrows))
+    maxpos_WEST = sheet.col_values(8, 1, sheet.nrows-1).index(maxval_WEST) + 1
     maxtime_WEST = xlrd.xldate_as_tuple(sheet.cell_value(maxpos_WEST, 0), 0)
-
+    tup_WEST  = (maxtime_WEST)
+    max_YEAR_WEST  = tup_WEST[0]
+    max_MONTH_WEST  = tup_WEST[1]
+    max_DAY_WEST  = tup_WEST[2]
+    max_HOUR_WEST = tup_WEST[3]
 
     #print(maxtime_COAST)
     # YOUR CODE HERE
-    data = {'COAST': {'Max Load': maxval_coast, 'Time': maxtime_COAST},
-             'EAST': {'Max Load': maxval_EAST, 'Time': maxtime_EAST},
-             'FAR_WEST': {'Max Load': maxval_FAR_WEST, 'Time': maxtime_FAR_WEST},
-             'NORTH': {'Max Load': maxval_NORTH, 'Time':maxtime_NORTH},
-             'NORTH_C': {'Max Load': maxval_NORTH_C, 'Time': maxtime_NORTH_C},
-             'SOUTHERN': {'Max Load': maxval_SOUTHERN,'Time': maxtime_SOUTHERN},
-             'SOUTH_C': {'Max Load': maxval_SOUTH_C, 'Time': maxtime_SOUTH_C},
-             'West': {'Max Load': maxval_West, 'Time': maxtime_WEST}
+    data = {'COAST': {'Max Load': maxval_coast, 'Year': max_YEAR_coast, 'Month': max_MONTH_coast, 'DAY' : max_DAY_coast, 'Hour' : max_HOUR_coast},
+             'EAST': {'Max Load': maxval_EAST, 'Year': max_YEAR_EAST, 'Month': max_MONTH_EAST, 'DAY' : max_DAY_EAST, 'Hour' : max_HOUR_EAST},
+             'FAR_WEST': {'Max Load': maxval_FAR_WEST, 'Year': max_YEAR_FAR_WEST, 'Month': max_MONTH_FAR_WEST, 'DAY' : max_DAY_FAR_WEST, 'Hour' : max_HOUR_FAR_WEST},
+             'NORTH': {'Max Load': maxval_NORTH, 'Year': max_YEAR_NORTH, 'Month': max_MONTH_NORTH, 'DAY' : max_DAY_NORTH, 'Hour' : max_HOUR_NORTH},
+             'NORTH_C': {'Max Load': maxval_NORTH_C, 'Year': max_YEAR_NORTH_C, 'Month': max_MONTH_NORTH_C, 'DAY' : max_DAY_NORTH_C, 'Hour' : max_HOUR_NORTH_C},
+             'SOUTHERN': {'Max Load': maxval_SOUTHERN, 'Year': max_YEAR_SOUTHERN, 'Month': max_MONTH_SOUTHERN, 'DAY' : max_DAY_SOUTHERN, 'Hour' : max_HOUR_SOUTHERN},
+             'SOUTH_C': {'Max Load': maxval_SOUTH_C, 'Year': max_YEAR_SOUTH_C, 'Month': max_MONTH_SOUTH_C, 'DAY' : max_DAY_SOUTH_C, 'Hour' : max_HOUR_SOUTH_C},
+             'WEST': {'Max Load': maxval_WEST, 'Year': max_YEAR_WEST, 'Month': max_MONTH_WEST, 'DAY' : max_DAY_WEST, 'Hour' : max_HOUR_WEST}
             }
     # Remember that you can use xlrd.xldate_as_tuple(sometime, 0) to convert
     # Excel date to Python tuple of (year, month, day, hour, minute, second)
@@ -107,6 +148,7 @@ def test():
 
                     # Otherwise check for equality
                     else:
+                        print("STATION FIELD", ans[station][field], "LINE FIELD", line[field])
                         assert ans[station][field] == line[field]
 
             number_of_rows += 1
@@ -117,6 +159,7 @@ def test():
 
         # Check Station Names
         assert set(stations) == set(correct_stations)
+
 
         
 if __name__ == "__main__":
